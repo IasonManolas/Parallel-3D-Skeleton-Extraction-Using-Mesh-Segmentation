4f9462839f3c3924ff4a7d0a4fdc602f9c629ea3
IasonManolas <qqtolene@gmail.com>
2016-10-10 10:50:51 +0300

    Hello Triangle using GLEW modern OpenGL and Qt(without its opengl wrappers)

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QSurfaceFormat>
+#include <QOpenGLContext>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-
-
-w.show();
+    w.show();
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,19 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <qdebug.h>
+#include <QOpenGLContext>
 #include <QOpenGLWidget>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QOpenGLWidget *widget=ui->openGLWidget;
-    QSurfaceFormat format;
-    format.setDepthBufferSize(24);
-    format.setStencilBufferSize(8);
-    format.setVersion(3, 3);
-    format.setProfile(QSurfaceFormat::CoreProfile);
-    widget->setFormat(format);
-
+    QOpenGLWidget *myGLWidget=ui->openGLWidget;
 }
 
 MainWindow::~MainWindow()
