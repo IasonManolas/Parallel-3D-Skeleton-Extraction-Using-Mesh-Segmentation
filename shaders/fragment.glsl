eadef7fcd8c8ef0cbf5b20fa5b2f4b465706b0d8
IasonManolas <qqtolene@gmail.com>
2016-10-16 17:20:16 +0300

    Shader now uses directional light (aka light struct does not have a position but only a direction).

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -4,7 +4,8 @@ in vec3 FragPos;
 out vec4 color;
 
 struct Light {
-    vec3 position;
+    //vec3 position;
+    vec3 direction;
 
     vec3 ambient;
     vec3 diffuse;
@@ -33,7 +34,7 @@ void main()
 
     //diffuse
     vec3 norm=normalize(Normal);
-    vec3 lightDir=normalize(light.position-FragPos);
+    vec3 lightDir=normalize(-light.direction);
     float diff=max(dot(norm,lightDir),0.0);
     vec3 diffuse=light.diffuse*(diff*material.diffuse);
 
