7285505e79303eeed62fd158b4a6c2bf68caf581
Innora <iasonmanolasece@gmail.com>
2017-05-02 18:29:05 +0300

    Minor changes
    Former-commit-id: b8839bbda641dd108c12957c64d777ba70eca1f7

--- glwidget.cpp
+++ glwidget.cpp
@@ -4,127 +4,127 @@
 #include <QOpenGLContext>
 GLWidget::GLWidget(QWidget *parent) {
 
-  QOpenGLWidget *widget = this;
-  QSurfaceFormat format;
-  format.setDepthBufferSize(24);
-  format.setStencilBufferSize(8);
-  format.setVersion(4, 1);
-  format.setProfile(QSurfaceFormat::CoreProfile);
-  format.setOption(QSurfaceFormat::DebugContext);
+	QOpenGLWidget *widget = this;
+	QSurfaceFormat format;
+	format.setDepthBufferSize(24);
+	format.setStencilBufferSize(8);
+	format.setVersion(4, 1);
+	format.setProfile(QSurfaceFormat::CoreProfile);
+	format.setOption(QSurfaceFormat::DebugContext);
 
-  widget->setFormat(format);
+	widget->setFormat(format);
 
-  setFocusPolicy(Qt::StrongFocus);
+	setFocusPolicy(Qt::StrongFocus);
 }
 
 GLWidget::~GLWidget() {
-  makeCurrent();
-  delete modelShader;
-  delete axesShader;
-  disconnect(&timer, SIGNAL(timeout()), this, SLOT(update()));
+	makeCurrent();
+	delete modelShader;
+	delete axesShader;
+	disconnect(&timer, SIGNAL(timeout()), this, SLOT(update()));
 }
 void GLWidget::initializeGL() {
 
-  // Set this to true so GLEW knows to use a modern approach to retrieving
-  // function pointers and extensions
-  glewExperimental = GL_TRUE;
-  // Initialize GLEW to setup the OpenGL Function pointers
-  GLenum err = glewInit();
-  if (GLEW_OK != err) {
-    qDebug() << "[Error] GLEW failed to initialize. "
-             << (const char *)glewGetErrorString(err);
-  }
+	// Set this to true so GLEW knows to use a modern approach to retrieving
+	// function pointers and extensions
+	glewExperimental = GL_TRUE;
+	// Initialize GLEW to setup the OpenGL Function pointers
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		qDebug() << "[Error] GLEW failed to initialize. "
+			<< (const char *)glewGetErrorString(err);
+	}
 
-  glViewport(0, 0, WIDTH, HEIGHT);
+	glViewport(0, 0, WIDTH, HEIGHT);
 
-  glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 
-  char *renderer = (char *)glGetString(GL_RENDERER);
-  char *version = (char *)glGetString(GL_VERSION);
-  char *vendor = (char *)glGetString(GL_VENDOR);
+	char *renderer = (char *)glGetString(GL_RENDERER);
+	char *version = (char *)glGetString(GL_VERSION);
+	char *vendor = (char *)glGetString(GL_VENDOR);
 
-  std::cout << "version:" << version << std::endl;
-  std::cout << "renderer:" << renderer << std::endl;
-  std::cout << "vendor:" << vendor << std::endl;
-  // I have to constructors for class Shader in order
-  // to use the "real" one after the opengl context is active
-  // IS THIS SYNTAX OS dependant? build dir must be in the same folder(aka
-  // Projects) as the sources(aka OpenGL_WithoutWrappers)
-  modelShader = new Shader("../Thesis/shaders/vertex.glsl",
-                           "../Thesis/shaders/fragment.glsl");
-  axesShader = new Shader("../Thesis/shaders/axesvs.glsl",
-                          "../Thesis/shaders/axesfs.glsl");
+	std::cout << "version:" << version << std::endl;
+	std::cout << "renderer:" << renderer << std::endl;
+	std::cout << "vendor:" << vendor << std::endl;
+	// I have to constructors for class Shader in order
+	// to use the "real" one after the opengl context is active
+	// IS THIS SYNTAX OS dependant? build dir must be in the same folder(aka
+	// Projects) as the sources(aka OpenGL_WithoutWrappers)
+	modelShader = new Shader("../shaders/vertex.glsl",
+			"../shaders/fragment.glsl");
+	axesShader = new Shader("../shaders/axesvs.glsl",
+			"../shaders/axesfs.glsl");
 
-  scene.loadMesh("../Thesis/Models/test.obj");
-  scene.intersectionSphere.load("../Thesis/Models/icosahedron.obj",
-                                scene.P.averageEdgeLength / 5);
+	scene.loadMesh("../Models/test.obj");
+	scene.intersectionSphere.load("../Models/icosahedron.obj",
+			scene.P.averageEdgeLength / 5);
 
-  connect(&timer, SIGNAL(timeout()), this, SLOT(update()));
-  timer.start(30);
+	connect(&timer, SIGNAL(timeout()), this, SLOT(update()));
+	timer.start(30);
 }
 void GLWidget::resizeGL(int w, int h) {
-  WIDTH = w;
-  HEIGHT = h;
-  scene.updateProjectionMatrix(w, h);
+	WIDTH = w;
+	HEIGHT = h;
+	scene.updateProjectionMatrix(w, h);
 }
 
 void GLWidget::paintGL() {
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glShadeModel(GL_SMOOTH);
-  if (surfaceState == dontShowTriangles)
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-  else if (surfaceState == showTriangles)
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-  scene.P.updateVertexBuffer();
-  scene.Draw(modelShader, axesShader);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glShadeModel(GL_SMOOTH);
+	if (surfaceState == dontShowTriangles)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	else if (surfaceState == showTriangles)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	scene.P.updateVertexBuffer();
+	scene.Draw(modelShader, axesShader);
 }
 void GLWidget::mousePressEvent(QMouseEvent *event) {
-  // initialize mouse position
-  lastMousePos = QVector2D(event->localPos());
-  if (mode == pickVertex &&
-      scene.rayIntersectsPolyhedron(lastMousePos.x(), lastMousePos.y(), WIDTH,
-                                    HEIGHT)) {
-    scene.showIntersection = true;
-  }
+	// initialize mouse position
+	lastMousePos = QVector2D(event->localPos());
+	if (mode == pickVertex &&
+			scene.rayIntersectsPolyhedron(lastMousePos.x(), lastMousePos.y(), WIDTH,
+				HEIGHT)) {
+		scene.showIntersection = true;
+	}
 }
 
 void GLWidget::mouseMoveEvent(QMouseEvent *event) {
-  QVector2D mouseOffset = QVector2D(event->localPos()) - lastMousePos;
-  lastMousePos = QVector2D(event->localPos());
+	QVector2D mouseOffset = QVector2D(event->localPos()) - lastMousePos;
+	lastMousePos = QVector2D(event->localPos());
 
-  if (event->buttons() == Qt::LeftButton) {
-    if (mode == view) {
-      glm::vec2 mouseMoveOffset(mouseOffset.x(), mouseOffset.y());
-      scene.processMouseMovement(mouseOffset);
-    }
-    //    else if (mode == pick) {
-    //      scene.rayIntersectsPolyhedron(lastMousePos.x(), lastMousePos.y(),
-    //      WIDTH,
-    //                                    HEIGHT);
-    //    }
-  }
+	if (event->buttons() == Qt::LeftButton) {
+		if (mode == view) {
+			glm::vec2 mouseMoveOffset(mouseOffset.x(), mouseOffset.y());
+			scene.processMouseMovement(mouseOffset);
+		}
+		//    else if (mode == pick) {
+		//      scene.rayIntersectsPolyhedron(lastMousePos.x(), lastMousePos.y(),
+		//      WIDTH,
+		//                                    HEIGHT);
+		//    }
+	}
 }
 
 void GLWidget::wheelEvent(QWheelEvent *event) {
-  scene.camera.processWheelMovement(event->delta());
+	scene.camera.processWheelMovement(event->delta());
 }
 
 #include <thread>
 void GLWidget::keyPressEvent(QKeyEvent *event) {
-  switch (event->key()) {
+	switch (event->key()) {
 
-  case Qt::Key_M: {
-    mode = static_cast<Mode>(mode ^ 1);
+		case Qt::Key_M: {
+					mode = static_cast<Mode>(mode ^ 1);
 
-    break;
-  }
-  case Qt::Key_S: {
-    std::thread t(&MyPolyhedron::segmentMesh, scene.P);
-    t.join();
-    break;
-  }
-  }
+					break;
+				}
+		case Qt::Key_S: {
+					std::thread t(&MyPolyhedron::segmentMesh, scene.P);
+					t.join();
+					break;
+				}
+	}
 }
 // void GLWidget::keyReleaseEvent(QKeyEvent *event)
 //{
@@ -138,22 +138,22 @@ void GLWidget::keyPressEvent(QKeyEvent *event) {
 //}
 
 void GLWidget::modelWasChosen(std::__cxx11::string filename) {
-  makeCurrent();
-  scene.loadMesh(filename);
+	makeCurrent();
+	scene.loadMesh(filename);
 }
 
 void GLWidget::updateAxesState(bool state) { scene.setShowAxes(state); }
 
 void GLWidget::updateMeshSurfaceState(bool state) {
-  surfaceState = static_cast<meshSurfaceVizualization>(state);
+	surfaceState = static_cast<meshSurfaceVizualization>(state);
 }
 
 void GLWidget::resetCamera() {
-  std::cout << "camera reseted" << std::endl;
-  scene.camera.resetCamera();
-  scene.light.changeLightDirection(scene.camera.getPosition());
+	std::cout << "camera reseted" << std::endl;
+	scene.camera.resetCamera();
+	scene.light.changeLightDirection(scene.camera.getPosition());
 }
 
 void GLWidget::handleLoggedMessage(QOpenGLDebugMessage message) {
-  qDebug() << message;
+	qDebug() << message;
 }
